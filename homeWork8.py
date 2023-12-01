4cf2cf77df4f43d190083beaba516d601ee7e218
lifetreemkt <carlos.rodrigues@lifetreemarketing.com>
2023-12-01 13:16:20 -0300

    ok

--- homeWork8.py
+++ homeWork8.py
@@ -1,10 +1,11 @@
 
+# Função para desenha o tabuleiro
 def drawField():
     for row in range(5): #0,1,2,3,4
         if row % 2 == 0:
             for column in range(5):
                 if column % 2 == 0:
-                        if column !=4:
+                        if column != 4:
                             print (" ", end="")
                         else:
                             print(" ")
@@ -13,4 +14,11 @@ def drawField():
         else:
             print("-----")
 
-drawField()            
+print ("Welcome to Tic Tac Toe\nThis is our battlefield:");
+drawField()
+#starting the game
+while (True):
+    MoveRow = int(input("Enter row number please:"))
+    MoveColumn = int(input("Enter Column number please:"))
+    if Player == 1:
+        
